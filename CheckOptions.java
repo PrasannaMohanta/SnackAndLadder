dbc6bb65589d48501e13d5e10d66c53cda4a5f3e
“PrasannaMohanta” <pmohanta53@gmail.com>
2023-04-11 22:47:26 +0530

    Added:cheacking noplay,snack or ladder in the position

---
+++ CheckOptions.java
@@ -0,0 +1,75 @@
+package com.SnackLadderGame;
+public class CheckOptions {
+
+    int snake[] = { 12, 46, 57, 78 };
+    int ladder[] = { 13, 47, 58, 79 };
+
+    public static void main(String[] args) {
+        CheckOptions snakeAndLadderGame = new CheckOptions();
+        snakeAndLadderGame.snakeAndLadder();
+    }
+
+    public void snakeAndLadder() {
+        Player prasanna = new Player();
+        while(prasanna.getPostion() != 100) {
+            System.out.println("Position = "+prasanna.getPostion());
+            player(prasanna);
+        }
+
+        System.out.println("Position = "+prasanna.getPostion());
+    }
+
+    public void player(Player prasanna) {
+        int position = prasanna.getPostion();
+        int randomNum = getRandomNumber(6);
+        while (randomNum == 6 && position <= 94) {
+            position += randomNum;
+
+            if (isSnake(position)) {
+                position -= 5;
+                break;
+            } else if (isLadder(position)) {
+                position += 5;
+                randomNum = getRandomNumber(6);
+            } else {
+                randomNum = getRandomNumber(6);
+            }
+        }
+
+        if ((position <= 95 && randomNum == 5) || (position <= 96 && randomNum == 4)
+                || (position <= 97 && randomNum == 3) || (position <= 98 && randomNum == 2)
+                || (position <= 99 && randomNum == 1)) {
+            position += randomNum;
+            if (isSnake(position)) {
+                position -= 5;
+            } else if (isLadder(position) && position + 5 <= 100) {
+                position += 5;
+                randomNum = getRandomNumber(6);
+            }
+        }
+
+        prasanna.setPostion(position);
+    }
+
+    public boolean isSnake(int position) {
+        for (int i = 0; i < snake.length; i++) {
+            if (position == snake[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean isLadder(int position) {
+        for (int i = 0; i < ladder.length; i++) {
+            if (position == ladder[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int getRandomNumber(int value) {
+        return (int) Math.floor(Math.random() * value + 1);
+    }
+}
